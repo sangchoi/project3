be0fd634052182021380b2620b31b04990cc8ae8
FX-Wood <fxwood2@gmail.com>
2019-03-23 10:45:38 -0700

    GET /props

---
+++ routes/props.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Props = require('../models/props');
+
+router.route('/')
+    .get((req, res) => {
+        console.log('GET /props')
+        Props.find()
+        .exec()
+        .then(props => {
+            res.status(200).json({type: 'success', message: 'got props', data: props})
+        })
+        .catch(err => {
+            console.log('error')
+            res.status(500).json({type: 'error', message: err.message})
+        })
+    })
