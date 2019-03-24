c66ebc56ba2c1ee57d837cb35c30afa710b9a156
FX-Wood <fxwood2@gmail.com>
2019-03-24 10:28:57 -0700

    Added form control

--- client/src/SetUpPage.jsx
+++ client/src/SetUpPage.jsx
@@ -5,76 +5,100 @@ class SetUpPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            depFormValue: {},
-            peopleFormValue: {},
+            depName: '',
+            userName: '',
+            employeeId: '',
             users: [],
             deps: [],
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    componentDidMount() {
+        // get all departments
+
+        // get all people
+    }
 
     handleInputChange(e) {
         console.log('handleInputChange()')
         e.preventDefault()
-        console.log({ 
-            targetValue: e.target.value,
-            target: e.target.value,
-            e
+        const name = e.target.name;
+        const value = e.target.value;
+        console.log({ name, value })
+        this.setState({
+            [name]: value
         })
     }
 
     handleSubmit(e) {
         e.preventDefault()
-        console.log('formSubmit()')
-        console.log('depformsubmit')
-        console.log(e.target)
+        switch(e.target.name) {
+            case 'addDep':
+                this.addDep();
+                break;
+            case 'addUser':
+                this.addUser();
+                break;
+        }
     }
 
+    addDep() {
+        // axios to make a department
+        console.log(this.state.depName)
+    }
+    addUser() {
+        // axios to make a user
+        console.log(this.state.userName, this.state.employeeID)
+    }
     render() {
         return (
             <div className="SetUpPage">
                 <h1>Set up your Town</h1>
-                <div className="">
-                    <div className="Teams two-column">
-                        <div className="LeftTeams col1">
-                            <h3>Add Teams</h3>
-                            <form name="addDepartmentForm" onSubmit={ this.handleSubmit }>
-                                <input name="name" onChange={ this.handleInputChange } type="text" placeholder="Enter team name here..."/>
-                                <br/>
-                                <input onSubmit={ this.handleSubmit } type="submit" value="Create Department"/>
-                            </form>
-                        </div>
-                        <div className="RightTeams col2">
-                            added teams should show up here
-                            <ul>
-                                <li>DevOps</li>
-                                <li>Sales</li>
-                                <li>IT</li>
-                                <li>Marketing</li>
-                            </ul>
-                        </div>
+                <div className="Teams two-column">
+                    <div className="LeftTeams col1">
+                        <h3>Add Teams</h3>
+                        <form name="addDep" onSubmit={ this.handleSubmit }>
+                            <input 
+                                name="depName" 
+                                onChange={ this.handleInputChange } 
+                                type="text" 
+                                placeholder="Enter team name here..."/>
+                            <br/>
+                            <input 
+                                onSubmit={ this.handleSubmit } 
+                                type="submit" 
+                                value="Create Department"/>
+                        </form>
                     </div>
-                    <div className="People two-column">
-                        <div className="LeftPeople col1">
-                            <h3>Add People</h3>
-                            <form name="addPeopleForm" onSubmit={ this.handleSubmit }>
-                                <input onChange={ this.handleInputChange } type="text" placeholder="Enter name" name="name"/>
-                                <br/>
-                                <input onChange={ this.handleInputChange } type="text" placeholder="Enter employee ID here" name="employeeID"/>
-                                <p>Helpful hint: Employee ID is the number that employees will use to sign in for the first time</p>
-                                <input type="submit" value="Create Department"/>
-                            </form>
-                        </div>
-                        <div className="RightPeople col2">
-                            <ul>
-                                <li>I'm a person you made</li>
-                                <li>Me too!</li>
-                            </ul>
-                        </div>
+                    <div className="RightTeams col2">
+                        added teams should show up here
+                        <ul>
+                            <li>DevOps</li>
+                            <li>Sales</li>
+                            <li>IT</li>
+                            <li>Marketing</li>
+                        </ul>
                     </div>
                 </div>
-                <button className="checkToken">CHECK THE TOKEN USING THAT ONE ROUTE</button>
+                <div className="People two-column">
+                    <div className="LeftPeople col1">
+                        <h3>Add People</h3>
+                        <form name="addUser" onSubmit={ this.handleSubmit }>
+                            <input onChange={ this.handleInputChange } type="text" placeholder="Enter name" name="userName"/>
+                            <br/>
+                            <input onChange={ this.handleInputChange } type="text" placeholder="Enter employee ID here" name="employeeID"/>
+                            <p>Helpful hint: Employee ID is the number that employees will use to sign in for the first time</p>
+                            <input type="submit" value="Create Department"/>
+                        </form>
+                    </div>
+                    <div className="RightPeople col2">
+                        <ul>
+                            <li>I'm a person you made</li>
+                            <li>Me too!</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
