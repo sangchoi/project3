d8e9333a9aa75efab8636f51d3277f5017da0753
FX-Wood <fxwood2@gmail.com>
2019-03-25 13:14:33 -0700

    Adding browser router boilerplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 // npm packages
 import axios from 'axios';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 // REACT COMPONENTS
 // AUTH
@@ -140,9 +141,11 @@ class App extends Component {
     }
     return (
       <div className="App">
+      <BrowserRouter>
         <header><h1>Welcome to my site!</h1></header>
         <h3>{ this.state.message }</h3>
         {content}
+      </BrowserRouter>
       </div>
     )
   }
