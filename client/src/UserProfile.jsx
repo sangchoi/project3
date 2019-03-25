274c49d8363252fad61e4ea5daf043632c1c5527
FX-Wood <fxwood2@gmail.com>
2019-03-25 00:00:59 -0700

    Reverted the logout button that was removed by PR#27

--- client/src/UserProfile.jsx
+++ client/src/UserProfile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function UserProfile({ user }) {
+export default function UserProfile({ user, logout }) {
     return (
         <div className="Profile">     
-            <h2>Profile</h2>
+            <button onClick={ logout }>Log Out!</button>
+            <h2>{ user.name}'s Profile</h2>
             <img src="https://via.placeholder.com/150" alt="user avatar large"/>
             <p>User name: { user.name }</p>
             <p>User email: { user.email }</p>
