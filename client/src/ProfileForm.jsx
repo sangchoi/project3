38679232913c8b02fcd95e1733cc3c2fdef1f877
FX-Wood <fxwood2@gmail.com>
2019-03-26 16:05:46 -0700

    Hooking up profile form to API

--- client/src/ProfileForm.jsx
+++ client/src/ProfileForm.jsx
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class ProfileForm extends Component {
+    // props include 'user' from app.jsx
     constructor(props) {
         super(props)
         this.state = {
             deps: [],
             dep: '',
+            groups: '',
             interests: '',
             photo: '',
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleInputChange(e) {
         this.setState({
             [e.target.name]: e.target.value 
         })
     }
+
     handleSubmit(e) {
         e.preventDefault()
         console.log(this.state)
-        
+        // add this user to department
+        console.log('/api/deps/' + this.state.dep)
+        axios.put('/api/deps/' + this.state.dep, { 
+            members: [this.props.user._id],
+        })
+        // make profile and add it to user
+        console.log('/api/profile')
+        console.log({
+            interests: this.state.interests,
+            groups: this.state.groups,
+            photo: this.state.photo,
+            userId: this.props.user._id
+        })
+        axios.post('/api/profile', {
+            interests: this.state.interests,
+            groups: this.state.groups,
+            photo: this.state.photo,
+            userId: this.props.user._id
+        })
+
     }
+
     componentDidMount() {
         axios.get('/api/deps')
         .then(res => {
@@ -32,13 +57,14 @@ class ProfileForm extends Component {
             }))
         })
     }
+
     render() {
-        let options = this.state.deps.map( dep => <option value={ dep._id } >{ dep.name }</option> )
+        let options = this.state.deps.map( (dep, i) => <option value={ dep._id } key={ i } >{ dep.name }</option> )
         return (
             <div className="ProfileForm">
                 <h3>Make A Profile</h3>
                 <form onSubmit={ this.handleSubmit }>
-                    <input onChange={ this.handleInputChange } type="text" placeholder="Enter a photo url here" />
+                    <input name="photo" onChange={ this.handleInputChange } type="text" placeholder="Enter a photo url here" />
                     <br/>
                     <select name="dep" id="dep" onChange={ this.handleInputChange }>
                         <option value="" hidden="hidden">Choose your Community</option>
