88415ba79e41283eaefc59c7de6413a18b5ae07e
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-03-26 12:31:25 -0700

    fonts and logo added

--- client/src/PropsLogo.jsx
+++ client/src/PropsLogo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import logo from './imgs/CMYK-Red-Black_Small_GeneralAssembly-Stacked.png'
 
 
 const PropsLogo = props => {
     return (
-
+        <React.Fragment>
         <header className="PropsLogoText"><h1>Props</h1></header>
+        <img className="GeneralAssemblyLogo" src={logo} alt="Logo" />
+        </React.Fragment>
     )
 
 }
