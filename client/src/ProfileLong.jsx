abd7eea29863f154a88c54e05e0aad62eade310f
FX-Wood <fxwood2@gmail.com>
2019-03-27 11:40:17 -0700

    Fixed old references in profile page components

--- client/src/ProfileLong.jsx
+++ client/src/ProfileLong.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 
-const ProfileLong = props => {
-    return (
-        <div className="profile-long">
-            <p>Interests: { profile.interests }</p>
-            <p>Groups: { profile.groups } </p>
-        </div>
-    )
+class ProfileLong extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            userAndProfile: {}
+        }
+    }
+    componentDidMount() {
+        console.log(this.props.user)
+        axios.get('/api/user/' + this.props.user._id + '/profile')
+        .then(res => {
+            console.log(res.data.data)
+            this.setState({
+                userAndProfile: res.data.data
+            })
+        })
+    }
+    render() {
+        let content;
+        if ( 'profile' in this.state.userAndProfile) {
+            content = (
+                <>
+                    <p>Interests: { this.state.userAndProfile.profile.interests }</p>
+                    <p>Groups: { this.state.userAndProfile.profile.interests } </p>
+                </>
+            )
+        } else {
+            content = (
+                null
+            )
+        }
+        return (
+            <div className="profile-long">
+                { content }
+            </div>
+        )
+    }
 }
 
 export default ProfileLong;
