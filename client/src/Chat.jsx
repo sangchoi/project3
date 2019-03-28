cd59662cf7c8d27f960ee34d75a89e6c0a3b184e
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2019-03-27 17:03:36 -0700

    set up for fix of textarea input

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import CreateChat from './CreateChat';
+import './Chat.css';
 
 class Chat extends Component {
     constructor(props) {
@@ -13,6 +14,7 @@ class Chat extends Component {
         }
     }
     componentDidMount() {
+        this.getMessages()
         let pollingInterval = setInterval(this.getMessages, 1000)
         this.setState({
             pollingInterval
@@ -27,18 +29,23 @@ class Chat extends Component {
             body: body
         })
     }
+    clearTextArea = () => {
+        let body = this.state.body
+        this.state.onSearchTermChange(body)
+        this.setState({
+            body: ''
+        })
+    }
     handleSubmit = (e) => {
-        console.log(this.props.user._id)
         e.preventDefault()
         let data = {
             senderId: this.props.user._id,
             body: this.state.body
         }
-        console.log("checking user id again:", data.senderId)
         axios.post('/api/chat', data)
         .then(res => {
-            console.log('chat data sent to server', res)
             this.getMessages()
+            this.clearTextArea()
         }).catch(err => {
             console.log("error returned from axios:", err)
         })
@@ -48,8 +55,8 @@ class Chat extends Component {
             .then(res => {
                 this.setState({
                     messages: res.data.data
-                });
-            });
+                })
+            })
     }
     render() {
         let chatFeed = this.state.messages.map(( message, index) => {
@@ -57,12 +64,16 @@ class Chat extends Component {
         })
         console.log(chatFeed)
         return ( 
-            <div>
+            <>
+            <div className='chat-feed'>
                 <ul>
                     {chatFeed}
-                    <CreateChat user={this.props.user} handleSubmit={this.handleSubmit} handleTextArea={this.handleTextArea}  />
                 </ul>
             </div>
+            <div className='chat-input'>
+                    <CreateChat user={this.props.user} handleSubmit={this.handleSubmit} handleTextArea={this.handleTextArea} body={this.state.body} />
+            </div>
+            </>
         )
     }
 }
--- client/src/CreateChat.jsx
+++ client/src/CreateChat.jsx
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-class CreateChat extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-        }
-    }
-    render() {
-        return(
-            <div className='chat-input'>
-                <h4>Chat</h4>
-                <form onSubmit={this.props.handleSubmit}>
-                    <textarea name='chat-body' placeholder='text...' onChange={ this.props.handleTextArea } />
-                    <input type='submit' value='chat' />
-                </form>
-            </div>
-        )
-    }
+const CreateChat = (props) => {
+    return (
+        <div>
+            <h4>Chat</h4>
+            <form onSubmit={props.handleSubmit}>
+                <textarea name='chat-body' value={props.body} onChange={ props.handleTextArea } />
+                <input type='submit' value='chat' />
+            </form>
+        </div>
+    )
 }
 
 export default CreateChat;
