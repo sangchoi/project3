2e74386994768bbc14a10990ed2a827a53f41d70
SANG CHOI <sangchoi@SANGs-MacBook-Pro.local>
2019-03-27 23:13:28 -0700

    chat template

--- client/src/CreateChat.jsx
+++ client/src/CreateChat.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 
 const CreateChat = (props) => {
     return (
-            <div className='chat-input'>
+            <div className='ChatInputBox'>
                 <h4 className="ChatHeader">CHAT</h4>
                 <form className="ChatForm" onSubmit={props.handleSubmit}>
                     <textarea className="ChatTextBox" name='chat-body' placeholder='text...' onChange={ props.handleTextArea } />
-                    
                     <input className="ChatButton" type='submit' value='SUBMIT' />
                 </form>
             </div>
